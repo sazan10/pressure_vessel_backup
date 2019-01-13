cfb771e3bae5a846b5abedca10fd9811292429b1
Swainstha <swainstha@outlook.com>
2019-01-13 14:17:09 +0545

    the dropdown menu was reduced to a map function iterating through the json file

--- src/UI/DropDownMenu.js
+++ src/UI/DropDownMenu.js
@@ -1,64 +1,22 @@
-import React, { Component } from 'react';
-import DropDown from './DropDown';
-import './DropDownMenu.css';
-import  * as data from '../JSONFiles/File.json';
+import React, { Component } from "react";
+import DropDown from "./DropDown";
+import "./DropDownMenu.css";
+import * as data from "../JSONFiles/File.json";
 class DropDownMenu extends Component {
-
-    render() {
-        return (
-            <div className= "body">
-                <DropDown
-                    title="File"
-                    list={data.Files}
-                />
-                <DropDown
-                    title="Component"
-                    list={data.Component}
-                />
-                <DropDown
-                    title="Actions"
-                    list={data.Actions}
-                />
-                <DropDown
-                    title="Nozzle"
-                    list={data.Nozzle}
-                />
-                <DropDown
-                    title="Attach"
-                    list={data.Attach}
-                />
-                <DropDown
-                    title="Support"
-                    list={data.Support}
-                />
-                <DropDown
-                    title="Codes"
-                    list={data.Codes}
-                />
-                <DropDown
-                    title="Loads"
-                    list={data.Loads}
-                />
-                <DropDown
-                    title="Materials"
-                    list={data.Materials}
-                />
-                <DropDown
-                    title="Forms"
-                    list={data.Forms}
-                />
-                <DropDown
-                    title="Window"
-                    list={data.Window}
-                />
-                <DropDown
-                    title="Help"
-                    list={data.Help}
-                /> 
-
-            </div>
-        );
-    }
+  render() {
+    const menu = data.menu.map(d => 
+    //   let de = Object.keys(d);
+      <DropDown
+        key = {Object.keys(d)}
+        title = {Object.keys(d)}
+        list = {d[Object.keys(d)]} />
+    );
+    return (
+      <div className="body">
+        {menu}
+      </div>
+    );
+  }
 }
 
 export default DropDownMenu;
