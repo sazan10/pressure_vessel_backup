6f2fb0abc3556ba9e53a4af69d39c09479c612c9
Swainstha <swainstha@outlook.com>
2019-04-16 16:18:56 +0545

    saddle and skirt disable enable and distance for lifting lug

--- src/Components/UI/IconNavbar/IconNavbar.js
+++ src/Components/UI/IconNavbar/IconNavbar.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import classes from './IconNavbar.css';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import classes from "./IconNavbar.css";
 
-const IconNavbar = (props) => {
-    return (
-        <Button onClick={() => props.selectItem(props.title)} className={classes.Icons1}>
-            {props.title}
-            {/* <img src={a} alt="Fuck Pritesh" height="30px" width="30px"/> */}
-        </Button>
-    );
-}
+const IconNavbar = props => {
+  let but = (
+    <Button
+      onClick={() => props.selectItem(props.title)}
+      className={classes.Icons1}
+    >
+      {props.title}
+      {/* <img src={a} alt="Fuck Pritesh" height="30px" width="30px"/> */}
+    </Button>
+  );
+  if (
+    (props.title === "Saddle" && props.orientation === "vertical") ||
+    (props.title === "Skirt" && props.orientation === "horizontal")
+  ) {
+    but = null;
+  }
+  return (
+    but
+  )  ;
+};
 
 export default IconNavbar;
